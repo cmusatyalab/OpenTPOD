ec1f97c12ba73296a88b6d73e27cb089009bf0f5
junjuew <junjuew@cs.cmu.edu>
2019-11-01 21:04:26 -0400

    [object-detector] add in tfod

---
+++ opentpod/object_detector/provider/tfod.py
@@ -0,0 +1,19 @@
+"""Tensorflow Object Detection API provider
+"""
+
+
+class TFODDetector():
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def prepare(self):
+        pass
+
+    def load(self):
+        pass
+
+    def train(self):
+        pass
+
+    def run(self):
+        pass
