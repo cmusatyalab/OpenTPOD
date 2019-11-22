de2f2c7fdf051e984d917b7702d43d820ec82fea
Jamesjue <canjian.myself@gmail.com>
2019-11-22 11:21:53 -0500

    [objectdetector] make nested field readable by full object, writable only through id

--- opentpod/object_detector/serializers.py
+++ opentpod/object_detector/serializers.py
@@ -8,10 +8,37 @@ import shutil
 from rest_framework import serializers
 from opentpod.object_detector import models
 from cvat.apps.engine.serializers import WriteOnceMixin
+from cvat.apps.engine.serializers import LabelSerializer, SegmentSerializer
+from cvat.apps.engine import models as cvat_models
+
+
+class SimpleTaskSerializer(serializers.ModelSerializer):
+    """A simplied task serializer for showing task information.
+    """
+    labels = LabelSerializer(many=True, source='label_set', partial=True, read_only=True)
+    image_quality = serializers.IntegerField(min_value=0, max_value=100, read_only=True)
+
+    class Meta:
+        model = cvat_models.Task
+        fields = ('id', 'name', 'size', 'mode', 'owner', 'assignee',
+                  'bug_tracker', 'created_date', 'updated_date', 'overlap',
+                  'status', 'labels', 'image_quality', 'start_frame',
+                  'stop_frame', 'frame_filter')
+        read_only_fields = ('name', 'size', 'mode', 'owner', 'assignee',
+                            'bug_tracker', 'created_date', 'updated_date', 'overlap',
+                            'status', 'labels', 'image_quality', 'start_frame',
+                            'stop_frame', 'frame_filter')
+        ordering = ['-id']
 
 
 class TrainSetSerializer(serializers.ModelSerializer):
-    # tasks = serializers.StringRelatedField(many=True)
+    tasks = SimpleTaskSerializer(many=True, read_only=True)
+    tasks_id = serializers.PrimaryKeyRelatedField(
+        source='tasks',
+        many=True,
+        queryset=models.Task.objects.all(),
+        write_only=True)
+
     class Meta:
         model = models.TrainSet
         fields = '__all__'
@@ -19,17 +46,21 @@ class TrainSetSerializer(serializers.ModelSerializer):
 
 
 class DetectorSerializer(WriteOnceMixin, serializers.ModelSerializer):
+    # a workaround to return full object when read
+    # but only needs object id for write.
+    # Otherwise, such behavior needs to be manually implemented in create()
+    # By default, drf doens't automatically implement nested writable fields.
+    # see: https://github.com/encode/django-rest-framework/issues/5206
+    train_set = TrainSetSerializer(read_only=True)
+    # the 'id' field of 'train_set'
+    train_set_id = serializers.PrimaryKeyRelatedField(
+        source='train_set',
+        queryset=models.TrainSet.objects.all(),
+        write_only=True
+    )
+
     class Meta:
         model = models.Detector
         fields = '__all__'
         read_only_fields = ('created_date', 'updated_date')
         write_once_fields = ('dnn_type', 'parent', 'train_set', 'train_config')
-
-    # def create(self, validated_data):
-    #     """Override create to create the nested trainconfig db entry.
-    #     This is called before the perform_create in views.py
-    #     """
-    #     trainconfig_data = validated_data.pop('trainconfig')
-    #     db_detector = models.Detector.objects.create(**validated_data)
-    #     models.TrainConfig.objects.create(detector=db_detector, **trainconfig_data)
-    #     return db_detector
