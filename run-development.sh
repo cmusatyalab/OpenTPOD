35ed899f160650f080aad46d253bc283eacc9c1d
Jan Harkes <jaharkes@cs.cmu.edu>
2020-07-30 14:40:47 -0400

    Docker-compose updates
    - extends was dropped from compose v3, using yaml anchors now.
    - tag built development image as opentpod:latest.
    - hardcode database name and and correct redis/cache/database urls
    - drop opentpod dependency from worker so it won't force a restart
    - remove quotes from .env example as they are interpreted as being
      part of the variables.
    - Fix database url check in run-development.

--- run-development.sh
+++ run-development.sh
@@ -1,8 +1,11 @@
-#!/bin/bash -l
+#!/bin/bash
 
 set -e
 
-test -n "$DATABASE_URL" || echo "Missing DATABASE_URL" && exit 0
+if [ -z "$DATABASE_URL" ] ; then
+    echo "Missing DATABASE_URL"
+    exit 1
+fi
 
 # Update conda environment
 conda env update -f requirements/environment.yml --prune
@@ -27,4 +30,4 @@ printf "Starting opentpod\n"
 # SSH_AUTH_SOCK to prevent CVAT from blocking when generating ssh keys
 export SSH_AUTH_SOCK="fix"
 /usr/bin/env python manage.py migrate && \
-    /usr/bin/env gunicorn config.wsgi:application --log-file - --log-level debug -b :8000"
+    /usr/bin/env gunicorn config.wsgi:application --log-file - --log-level debug -b :8000
--- run-worker.sh
+++ run-worker.sh
@@ -4,4 +4,4 @@ set -e
 
 # SSH_AUTH_SOCK to prevent CVAT from blocking when generating ssh keys
 export SSH_AUTH_SOCK="fix"
-command=/usr/bin/env python manage.py rqworker $*
+/usr/bin/env python manage.py rqworker $*
